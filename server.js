218e14d4f5d80e0d8c54503cf86e551c9a08da4f
Geevaveeri <59940979+Geevaveeri@users.noreply.github.com>
2021-06-28 21:12:08 -0600

    Update server.js

--- server.js
+++ server.js
@@ -37,12 +37,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(require("./controllers/"));
 
-app.use(function(req,res, next){
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-})
 
 // passport initializing
 app.use(passport.initialize());
